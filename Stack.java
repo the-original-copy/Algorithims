f264c0816f160b6cb0b16f6734d6e8b83dc97c70
the-original-copy <emuriithi1968@gmail.com>
2021-10-09 11:44:55 +0300

    Stack data structure

---
+++ Stack.java
@@ -0,0 +1,39 @@
+
+public class Stack<T> implements Iterable<T> {
+	private java.util.LinkedList<T> list = new java.util.LinkedList<T>();
+	
+	//Create an empty stack
+	public Stack() {}
+
+	//Create stack with an initial element
+	public Stack(T firstElem) {
+		push(firstElem);
+	}
+	
+	public int size() {
+		return list.size();
+	}
+	
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+	
+	public void push(T elem) {
+		list.addLast(elem);
+	}
+	
+	public T pop() {
+		if (isEmpty()) throw new java.util.EmptyStackException();
+		return list.removeLast();
+	}
+	
+	public T peek() {
+		if (isEmpty()) throw new java.util.EmptyStackException();
+		return list.peekLast();
+	}
+	
+	@Override
+	public java.util.Iterator<T> iterator(){
+		return list.iterator();
+	}
+}
